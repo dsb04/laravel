d89ca5a928940c76678962d011d571b828d50ace
Diogo <bdogo212@gmail.com>
2022-02-02 15:27:31 -0300

    l

---
+++ app/Services/PostService.php
@@ -0,0 +1,40 @@
+<?php
+ 
+namespace App\Services;
+
+use App\Models\Post;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\DB;
+use App\Repositories\PostRepository;
+use Illuminate\Support\Facades\Storage;
+
+class PostService
+{
+    /**
+     * @var PostRepository
+     */
+    protected $repository;
+    public function __construct(PostRepository $repository)
+    {
+        $this->repository = $repository;
+
+    }
+    public function store(array $input, UploadedFile $photo)
+    {
+        DB::beginTransaction();
+            try {
+                $path = $photo->store('public/images');
+                $url= Storage::url($path);
+
+                $post = $this->repository->create([
+                    'image' =>$url,'description'=>$input['description'],'user_id'=>$input['user_id']]);
+ } 
+            catch (\Throwable $th) {
+                DB::rollback();
+                logger()->error($th);
+                return ['success'=> false,'message'=>'Erro ao gravar post'];
+ }
+            DB::commit();
+            return['success'=>true,'message'=>'Post crado com sucesso','data'=>$post];
+    }
+}
